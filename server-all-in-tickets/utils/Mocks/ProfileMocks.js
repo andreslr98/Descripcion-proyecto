087e512bccbd86ac7cc6e176ac98a26d3a146226
sala 304 <sala304@poliacademico.edu.loc>
2019-07-25 10:30:43 -0500

    backend

---
+++ server-all-in-tickets/utils/Mocks/ProfileMocks.js
@@ -0,0 +1,41 @@
+const profileMocks = [
+    {
+        nombre: "Andres",
+        apellidos:"Lopez",
+        cedula:"4535434533534",
+        correo:"andres_lopez82161@elpoli.edu.co",
+        contraseña:"poli123",
+        direccion:"calle 32 #31-22",
+        telefono:"322343453",
+
+
+    },
+
+    {
+        nombre: "camilo",
+        apellidos:"colorado",
+        cedula:"455353534",
+        correo:"camilo_colorado82162@elpoli.edu.co",
+        contraseña:"poli123",
+        direccion:"calle 32 #31-244",
+        telefono:"3543453444",
+
+
+    },
+
+    {
+        nombre: "pablo",
+        apellidos:"orozoco",
+        cedula:"34534534554",
+        correo:"pablo_orozco82162@elpoli.edu.co",
+        contraseña:"poli123",
+        direccion:"calle #22 31-244",
+        telefono:"34443242342",
+
+
+    },
+
+
+]
+
+module.exports = profileMocks;
